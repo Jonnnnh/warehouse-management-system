f2e72550319325c15597140f31aa2b0c1f51de8d
Daria <jonshilin.gg@gmail.com>
2025-03-18 00:54:37 +0300

    feat: add user entity

---
+++ src/main/java/com/example/warehousemanagementsystem/entity/User.java
@@ -0,0 +1,80 @@
+package com.example.warehousemanagementsystem.entity;
+
+public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String email;
+    private String fullName;
+    private UserRole role;
+    private boolean active;
+
+    public User() {
+    }
+
+    public User(long id, String username, String password, String email, String fullName, UserRole role, boolean active) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullName = fullName;
+        this.role = role;
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
