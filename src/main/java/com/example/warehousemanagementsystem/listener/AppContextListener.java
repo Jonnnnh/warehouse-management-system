4c500729ac2f6d256af31ea66ae5fa342bc77ac6
Daria <jonshilin.gg@gmail.com>
2025-03-22 01:03:47 +0300

    refactor: update AppContextListener to delegate object creation to the factory

--- src/main/java/com/example/warehousemanagementsystem/listener/AppContextListener.java
+++ src/main/java/com/example/warehousemanagementsystem/listener/AppContextListener.java
@@ -1,17 +1,5 @@
 package com.example.warehousemanagementsystem.listener;
 
-import com.example.warehousemanagementsystem.config.DatabaseConfig;
-import com.example.warehousemanagementsystem.connection.ConnectionProvider;
-import com.example.warehousemanagementsystem.connection.DatabaseConnection;
-import com.example.warehousemanagementsystem.connection.DriverManagerStrategy;
-import com.example.warehousemanagementsystem.dao.InventoryOperationDao;
-import com.example.warehousemanagementsystem.dao.ProductDao;
-import com.example.warehousemanagementsystem.dao.UserDao;
-import com.example.warehousemanagementsystem.jdbc.Executor;
-import com.example.warehousemanagementsystem.service.OperationService;
-import com.example.warehousemanagementsystem.service.ProductService;
-import com.example.warehousemanagementsystem.service.UserService;
-
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
@@ -20,34 +8,15 @@ import jakarta.servlet.annotation.WebListener;
 @WebListener
 public class AppContextListener implements ServletContextListener {
 
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext ctx = sce.getServletContext();
-
-        String url = System.getenv("JDBC_URL");
-        String user = System.getenv("JDBC_USER");
-        String password = System.getenv("JDBC_PASSWORD");
-
         try {
-            ctx.log("Initializing AppContextListener with JDBC_URL: " + url);
-            DatabaseConfig dbConfig = new DatabaseConfig(url, user, password);
-            DriverManagerStrategy strategy = new DriverManagerStrategy();
-            String driverClassName = "org.postgresql.Driver";
-
-            ConnectionProvider connectionProvider = new DatabaseConnection(dbConfig, strategy, driverClassName);
-            Executor executor = new Executor(connectionProvider);
-
-            UserDao userDao = new UserDao(executor);
-            ProductDao productDao = new ProductDao(executor);
-            InventoryOperationDao opDao = new InventoryOperationDao(executor);
-
-            UserService userService = new UserService(userDao);
-            ProductService productService = new ProductService(productDao);
-            OperationService operationService = new OperationService(opDao, productDao);
-
-            ctx.setAttribute("userService", userService);
-            ctx.setAttribute("productService", productService);
-            ctx.setAttribute("operationService", operationService);
+            ApplicationContext appContext = ApplicationContextFactory.create();
+            ctx.setAttribute("userService", appContext.userService());
+            ctx.setAttribute("productService", appContext.productService());
+            ctx.setAttribute("operationService", appContext.operationService());
             ctx.log("AppContextListener initialization successful");
         } catch (Exception ex) {
             ctx.log("Error during AppContextListener initialization", ex);
